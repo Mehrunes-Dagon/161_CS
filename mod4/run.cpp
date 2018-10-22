7e6f2029d96d797c28882732104dd38b5e53e3b5
William <wtpwinberg@gmail.com>
2018-10-21 23:32:46 -0700

    Mod4 added

---
+++ mod4/run.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  string str1 = "English";
+  string str2 = "Computer Science";
+  string str3 = "Programming";
+  bool answer = str1 >= str2;
+
+  cout
+      << "answer:" << answer << ":" << endl;
+
+  return 0;
+}
