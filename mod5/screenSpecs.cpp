66592d7312acdd790d244c01a6aeda99cb8f62fc
William <wtpwinberg@gmail.com>
2018-10-26 23:43:22 -0600

    screenSpecs complete

--- mod5/screenSpecs.cpp
+++ mod5/screenSpecs.cpp
@@ -15,47 +15,85 @@ int main()
   float diagLength;
   int typeSelector;
   string type;
+  float width, height, area;
+  char reply;
 
-  do {
-    // get input from user
-    cout << "What is the diaganal length of the screen in inches" << endl;
+  do
+  {
+    // get size from user
+    cout << "What is the diaganal length of the screen in inches?" << endl;
     cin >> diagLength;
     // check for input failure
-    while (!cin) {
+    while (!cin)
+    {
       cin.clear();
       cin.ignore(100, '\n');
-      cout << "Enter valid input. (integer or decimal)";
+      cout << "Enter valid input. (integer or decimal)." << endl;
       cin >> diagLength;
     }
-
-    cout << "What type of screen would you like specs for? 1-Traditional; 2-HD; 3-Both (enter a single integer)" << endl;
+    // get screen type from user
+    cout << "What type of screen would you like specs for?" << endl
+         << "1.) Traditional" << endl
+         << "2.) HD" << endl
+         << "3.) Both" << endl;
     cin >> typeSelector;
-    while (!cin) {
+    // check for input failure
+    while (typeSelector < 1 || typeSelector > 3)
+    {
       cin.clear();
       cin.ignore(100, '\n');
-      cout << "Enter valid input. ('1', '2' or '3')";
+      cout << "Enter valid input. ('1', '2' or '3')" << endl;
       cin >> typeSelector;
     }
 
-    // assign type variable
-    switch (typeSelector) {
-      case 1: type = "Traditional TV";
-      case 2: type = "HD TV";
-      case 3: type = "Both types of TVs";
+    // list users requested specifications
+    while (typeSelector)
+    {
+      if (typeSelector == 1 || typeSelector == 3)
+      {
+        // calculate height and width using pathagorain theorem
+        // (4x)^2(3x)^2=diaglength^2
+        // 25x=bigNum
+        // ratio=bigNum/25
+        type = "Traditional TV";
+        float ratio = (diagLength * diagLength) / 25;
+        width = 4 * sqrt(ratio);
+        height = 3 * sqrt(ratio);
+        area = width * height;
+      }
+      else if (typeSelector == 2)
+      {
+        type = "HD TV";
+        float ratio = (diagLength * diagLength) / 337;
+        width = 16 * sqrt(ratio);
+        height = 9 * sqrt(ratio);
+        area = width * height;
+      }
+
+      // set precision and output result
+      cout << fixed << showpoint << setprecision(2);
+      cout << "The specifications of your " << type << " are:" << endl
+           << "Width: " << width << " inches" << endl
+           << "Height: " << height << " inches" << endl
+           << "Area: " << area << " square inches" << endl
+           << endl;
+
+      // if user wants to see both, restart loop to output option 2 as well
+      if (typeSelector == 3)
+      {
+        typeSelector = 2;
+        continue;
+      }
+      // break the loop if not option 3
+      break;
     }
 
+    // check if reapeat desired
+    cout << "Want to check another screen? (y/n)";
+    cin >> reply;
   } while (reply == 'y');
 
-  // // calculate formula
-  // carADistance = carASpeed * hours + (carASpeed * minutes / 60);
-  // carBDistance = carBSpeed * hours + (carBSpeed * minutes / 60);
-  // distanceBetween = sqrt((carADistance * carADistance) + (carBDistance * carBDistance));
-
-  // // set precision and output result
-  // cout << fixed << showpoint << setprecision(3);
-  // cout << "The distance between the cars after " << hours << " hours and " << minutes << " minutes is " << distanceBetween << " miles." << endl;
-
-  cout << "Press any key to quit";
+  cout << "Enter any key to quit";
   cin >> reply;
   return 0;
 }
