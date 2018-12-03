1ed8d251137eb3d9c20f383e6c45b26f3e77fae2
William <wtpwinberg@gmail.com>
2018-12-02 16:59:41 -0800

    getting there

--- mod8/averageTemps.cpp
+++ mod8/averageTemps.cpp
@@ -21,15 +21,13 @@ int main()
    int size = 0;
 
    ifstream inFile;
-   
-   cout << "This program will read a text file of months and cooresponding temeratures and output the highest and lowest temperatures for the year, and their corresponding months" << end;
+
+   cout << "This program will read a text file of months and cooresponding temperatures and output the highest and lowest temperatures for the year, and their corresponding months" << end;
 
    //open file
    openFile(inFile);
    //read from file
    size = readData(inFile, months, temperatures);
-   //calculate total and percentage for each candidate
-   calculateTotal(size, months, temperatures);
    return 0;
 }
 
@@ -53,7 +51,7 @@ int readData(ifstream &inFile, string months[], int temps[][2])
    int size = 0;
    while (!inFile.eof())
    {
-      inFile >> months[size] >> temps[size];
+      inFile >> months[size] >> temps[size][0] >> temps[size][1];
       size++;
    }
    return size;
@@ -67,20 +65,4 @@ int averageHigh(int temp[][2], int rows, int &hightemp, int &month)
 // finds and returns the low temperature and the corresponding month of the year
 int averageLow(int temp[][2], int rows, int &lowtemp, int &month)
 {
-}
-
-//calculate total and percentages
-void calculateTotal(int size, string names[], int votes[])
-{
-   int total = 0;
-   double percent = 0;
-   for (int i = 0; i < size; i++)
-      total += votes[i];
-   for (int i = 0; i < size; i++)
-   {
-      percent = (static_cast<double>(votes[i]) / total) * 100;
-      cout << fixed << showpoint << setprecision(2);
-      cout << left << setw(15) << names[i] << setw(8) << votes[i]
-           << setw(5) << percent << "%" << endl;
-   }
 }
